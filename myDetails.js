8300c381c6d59322d5f845eab42fbcac5ae958bb
Viashima <72166830+clins10@users.noreply.github.com>
2022-06-21 10:27:41 +0100

    added some changes

--- myDetails.js
+++ myDetails.js
@@ -1,10 +1,10 @@
-// const myDetails = () => {
-//     let myName = "Collins";
-//     let myHeight = 5.6;
-//     let myCountry = "Nigeria";
-//     let myDetails = 'hello 👋 nice to meet you \n' + "My name is " + myName +  " ," + " \nI' m " + myHeight + " feet tall," + "\nand I am from " + myCountry + " 😄😄" + "."
-//     return myDetails;
-// }
-// // console.log(myDetails());
-// alert(myDetails());
+const myDetails = () => {
+    let myName = "Collins";
+    let myHeight = 5.6;
+    let myCountry = "Nigeria";
+    let myDetails = 'Hello 👋 nice to meet you... \n' + "My name is " + myName +  " ," + " \nI' m " + myHeight + " feet tall," + "\nand I am from " + myCountry + " 😄😄" + "."
+    return myDetails;
+}
+// console.log(myDetails());
+alert(myDetails());
 
