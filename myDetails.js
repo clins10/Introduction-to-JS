4afd30a12e7ad72b11efef184a9c26ff2d4116c9
Viashima <72166830+clins10@users.noreply.github.com>
2022-06-21 01:38:21 +0100

    function to print my details on the screen

---
+++ myDetails.js
@@ -0,0 +1,8 @@
+//function myDetails() {
+//    var myName = "John";
+//    var myAge = "30";
+//    var myDetails = "My name is " + myName + " and I am " + myAge + " years old.";    
+//    return myDetails;
+//}
+//console.log(myDetails());
+
